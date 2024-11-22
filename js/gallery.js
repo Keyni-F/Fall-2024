d8d87fc754ca2947e76e753f76ebb479f17c7eaf
Keyni-F <keyni.farrow65@buc.blinn.edu>
2024-11-21 18:39:52 -0600

    added new gallery page

---
+++ js/gallery.js
@@ -0,0 +1,20 @@
+// A $( document ).ready() block.
+$(document).ready(function () {
+  // console.log( "ready!" );
+
+  $("#galleryIMED1316").nanogallery2({
+    thumbnailHeight: "auto",
+    thumbnailWidth: "450",
+    thumbnailBorderVertical: "0",
+    thumbnailBorderHorizontal: "0",
+    thumbnaillabel: {
+      position: "overImage0nBottom",
+      display: false,
+    },
+    thumbnailHoverEffect2: "imageScaleIn80|imgaeSepiaOff|LabelAppear75",
+    thumbnailGutterWidth: 20,
+    thumbnailGutterHeight: 20,
+    thumbnailAlignment: "center",
+    thumbnailOpenImage: true,
+  });
+});
